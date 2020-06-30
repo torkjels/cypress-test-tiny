d5229837605dab41d5444a3de40ee3338c4a866e
Torkjel Skjetne <3830142+torkjels@users.noreply.github.com>
2020-06-30 14:17:28 +0200

    Add comments

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -22,6 +22,15 @@ function doAssertion() {
     .should('contain', 'Engangskode')
 }
 
+/*
+ * These tests try to click through the iframes on the Norwegian BankID test page.
+ * BankID is the most commonly used electronic identification system in Norway ( https://www.bankid.no/en/private/about-us/ ).
+ *
+ * The tests come to a stop when the form validator in the iframe doesn't run when using Cypress to type.
+ *
+ * Only the first test runs green as it involves the user executing a keyboard action which makes the validator run
+ * which enables form submit.
+ */
 describe("iframe within iframe", () => {
   beforeEach(() => {
     cy.server()
@@ -48,7 +57,13 @@ describe("iframe within iframe", () => {
     // Click next button
     getInnerIframe()
       .find('button[title=Neste]')
-      .click({ force: true }) // Need to force
+      /*
+       * In order to click the "Next" button we need to use { force: true } or else an error would be thrown:
+       * "Timed out retrying: Failed to execute 'getComputedStyle' on 'Window': parameter 1 is not of type 'Element'."
+       * This is a different error than you get when an element is not present in the page, which would look like this
+       * "Timed out retrying: Expected to find element: button[title=Neste], but never found it. Queried from element: <body>"
+       */
+      .click({ force: true })
 
     // Wait for next frame to load
     cy.wait(3000)
@@ -60,7 +75,11 @@ describe("iframe within iframe", () => {
       .find('input[type=tel]')
       .type('11111111111', { force: true })
 
-    // Wait for user input (e.g. pressing an arrow key while having focus in the input field), then press the resume button
+    /*
+     * Pause to allow for user keyboard input (e.g. pressing an arrow key while having focus in the input field).
+     * When using { force: true } while typing, and then having the user execute a keyboard action in the input field
+     * before submit, the validator runs and submit is enabled.
+     */
     cy.pause()
 
     // Submit form
@@ -75,6 +94,10 @@ describe("iframe within iframe", () => {
     // Type into input
     getInnerIframe()
       .contains('Fødselsnummer')
+      /*
+       * Clicking the label to focus the input field allows for input without using force.
+       * But the validator is not run so the submit stays disabled.
+       */
       .click()
       .type('11111111111')
 
@@ -90,6 +113,7 @@ describe("iframe within iframe", () => {
     // Type into input
     getInnerIframe()
       .find('input[type=tel]')
+      // When using { force: true } while typing, the validator doesn't run and submit stays disabled.
       .type('11111111111', { force: true })
 
     // Submit form
@@ -104,6 +128,12 @@ describe("iframe within iframe", () => {
     // Type into input
     getInnerIframe()
       .find('input[type=tel]')
+      /*
+       * When using { force: false } while typing, nothing is typed into the input field and an error is thrown:
+       * "Timed out retrying: Failed to execute 'getComputedStyle' on 'Window': parameter 1 is not of type 'Element'."
+       * This is a different error than you get when an element is not present in the page, which would look like this
+       * "Timed out retrying: Expected to find element: input[type=tl], but never found it. Queried from element: <body>"
+       */
       .type('11111111111')
 
     // Submit form
